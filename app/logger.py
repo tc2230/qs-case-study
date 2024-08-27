4c15e2bab3cabbf62898e801eb40ca3ec1f58f46
tc2230 <root@TN-TP18S7J0V.taiwancement.com>
2024-08-27 14:16:16 +0800

    fix importing typo; remove redundant codes

--- app/logger.py
+++ app/logger.py
@@ -1,13 +1,9 @@
 import os
-# import dotenv
-import request
+import requests
 from google.cloud import logging
-
-dotenv.loads('.env')
 
 measurement_id = os.environ['MEASUREMENT_ID']
 api_secret = os.environ['API_SECRET']
-
 
 def setup_logger():
     client = logging.Client.from_service_account_json('app/gcp_logging_key.json')
@@ -17,9 +13,6 @@ def setup_logger():
     return logger
 
 def send_ga_record(payload):
-    # measurement_id = os.environ['MEASUREMENT_ID']
-    # api_secret = os.environ['API_SECRET']
-
     url = f'https://www.google-analytics.com/mp/collect?measurement_id={measurement_id}&api_secret={api_secret}'
 
     response = requests.post(url, data=json.dumps(payload), verify=True)
